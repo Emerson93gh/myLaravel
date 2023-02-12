5bb6fae409ae10b7610ccf7482358c61a0f4c6ca
Emerson Solano <emerson-solano@hotmail.com>
2023-02-12 07:36:04 -0600

    constuctor property promotion

--- LaravelPOO/06.php
+++ LaravelPOO/06.php
@@ -1,2 +1,23 @@
 <?php
 include 'includes/header.php';
+
+// Constructor property promotion, comparar con 04.php
+class Empleado {
+    public function __construct(
+        public $nombre,
+        public $apellido,
+        public $departamento,
+        public $email,
+        public $codigo,
+    ){}
+}
+// instancias
+$empleado = new Empleado('Emerson', 'Solano', 'TI', 'esolano@mail.com', 026);
+$empleado2 = new Empleado('Raquel', 'Luz', 'COM', 'rluz@mail.com', 027);
+
+echo "<pre>"; // dar mejor formato
+var_dump($empleado);
+echo "</pre><br/>";
+echo "<pre>"; // dar mejor formato
+var_dump($empleado2);
+echo "</pre>";
