33a8c258a14da4d9024712f5a88839bd93449ec2
Emerson Solano <emerson-solano@hotmail.com>
2023-03-25 16:41:58 -0600

    fix controller

--- cursolvl9/resources/views/blog.blade.php
+++ cursolvl9/resources/views/blog.blade.php
@@ -3,4 +3,7 @@ title="Blog"
 meta-description="Blog meta description">
 
     <h1>Blog</h1>
+    @foreach ($posts as $post)
+        <h3> {{ $post['title'] }} </h3>
+    @endforeach
 </x-layouts.app>
