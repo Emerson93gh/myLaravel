30f19b5e72e4aa04e5898c504e21ef90de04c90a
Emerson Solano <emerson-solano@hotmail.com>
2023-03-25 16:48:49 -0600

    conexion a base de datos

--- cursolvl9/app/Http/Controllers/PostController.php
+++ cursolvl9/app/Http/Controllers/PostController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index(){
-        $posts = [
-            ['title' => 'First post'],
-            ['title' => 'Second post'],
-            ['title' => 'Third post'],
-            ['title' => 'Fourth post'],
-        ];
+        // $posts = [
+        //     ['title' => 'First post'],
+        //     ['title' => 'Second post'],
+        //     ['title' => 'Third post'],
+        //     ['title' => 'Fourth post'],
+        // ];
+
+        // Mostrar datos de una tabla en la DB
+        $posts = DB::table('posts')->get();
 
         return view('blog', ['posts' => $posts]);
     }
--- cursolvl9/resources/views/blog.blade.php
+++ cursolvl9/resources/views/blog.blade.php
@@ -4,6 +4,7 @@ meta-description="Blog meta description">
 
     <h1>Blog</h1>
     @foreach ($posts as $post)
-        <h3> {{ $post['title'] }} </h3>
+        {{-- <h3> {{ $post['title'] }} </h3> --}}
+        <h3> {{ $post->title }} </h3>
     @endforeach
 </x-layouts.app>
