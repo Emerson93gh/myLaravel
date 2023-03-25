a5dd4b138d6eb1eb3daed7759d2668e72fb3fb36
Emerson Solano <emerson-solano@hotmail.com>
2023-03-25 15:14:52 -0600

    components blade

---
+++ cursolvl9/resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 - {{ $title ?? '' }} </title>
+    <meta name="description" content="{{$metaDescription ?? 'Default meta description'}})">
+    {{-- <link rel="icon" href="{{asset('favicon.ico')}}"> --}}
+</head>
+<body>
+    {{-- @include('partials.navigation') --}}
+    <x-layouts.navigation />
+
+    {{ $slot }}
+
+</body>
+</html>
--- cursolvl9/resources/views/welcome.blade.php
+++ cursolvl9/resources/views/welcome.blade.php
@@ -1,8 +1,20 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('title', 'Home')
 @section('meta-description', 'Home meta description')
 
 @section('content')
     <h1>Inicio</h1>
-@endsection
+@endsection --}}
+
+{{-- @component('components.layout')
+    <h1>Inicio</h1>
+@endcomponent --}}
+
+{{-- Otra forma --}}
+<x-layouts.app
+title="Home"
+meta-description="Home meta description">
+
+    <h1>Inicio</h1>
+</x-layouts.app>
